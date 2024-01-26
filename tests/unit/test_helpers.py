813e664c44f0bf1dae4fb0d54921394340f3fa87
miguel <mmedinaortiz20@wou.edu>
2024-01-25 22:24:32 -0800

    added passing test that checks if we get the correct response of just now if we pass in no information

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -6,3 +6,8 @@ from app.models import Category, Post, User
 
 def test_yesterday():
     assert (pretty_date(datetime.utcnow() - timedelta(days=1))) == "Yesterday"
+
+def test_today_just_now():
+    assert (pretty_date(datetime.utcnow())) ==  "just now"
+
+def 
